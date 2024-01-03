6d22cf2d2cc3d43a8d9e79ed45ac15c4b5b1ae4b
Jack Pflaum <jackpflaum@gmail.com>
2024-01-03 19:21:43 +0800

    created django models for image prompts, images, and user likes

--- Drawing_Of_The_Day/backend/drawings/models.py
+++ Drawing_Of_The_Day/backend/drawings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagePrompt(models.Model):
+    date = models.DateField()
+    prompt_text = models.TextField()
+
+    def __str__(self):
+        return f'Image Prompt: {self.date}'
+
+
+class Image(models.Model):
+    image_prompt = models.ForeignKey(ImagePrompt, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='images/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} uploaded image on {self.upload_date}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    like_status = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} liked or unliked image {self.image}'

de4fd24fa492a3340b19b16b78a56c5053dda7d0
Jack Pflaum <jackpflaum@gmail.com>
2024-01-04 18:38:56 +0800

    added useEffect to fetch data from django backend home view

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
@@ -1,11 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '.components/Header';
 import ImagesGrid from '.components/ImagesGrid';
 
 const HomeContent = () => {
+
+    const [ data, setData ] = useState({
+        date: '',
+        imagePrompt: '',
+        imagesList: [],
+    });
+
+    // initiate data fetching of image prompt and images.
+    useEffect(() => {
+        // 'async' keyword specifies that this is an asynchronous function that will return a promise (enables the use of 'await').
+        const fetchData = async () => {
+            try {
+                // 'await' keyword pauses the execution of the function until axios.get promise is resolved.
+                const response = await axios.get('http://localhost:8000/home/');
+                setData(response.data);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        }
+        fetchData();  //calls fetchData function when HomeContent component mounts
+    }, []);
+
     return(
         <>
-            <Header />
-            <ImagesGrid />
+            <Header date={data.date} imagePrompt={data.imagePrompt} />
+            <ImagesGrid imagesList={data.imagesList} />
         </>
     );
 };

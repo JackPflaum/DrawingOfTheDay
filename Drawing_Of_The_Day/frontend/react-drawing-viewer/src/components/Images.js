b91dc5b0c239036cf785d9e2dcad2c550cf9af62
Jack Pflaum <jackpflaum@gmail.com>
2024-01-07 12:34:07 +0800

    created layout for user images

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Images.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Images.js
@@ -1,9 +1,25 @@
-const Images = () => {
+import PropTypes from 'prop-types';
+
+const Images = ({ imagesList }) => {
     return (
-        <div>
-            
+        <div className="container">
+            <div className="row">
+                {imagesList ? (
+                    imagesList.map((image) => (
+                        <div key={image.id} className="col-md-4 mb-3">
+                            <img src={image.url} alt={image.id} className="img-fluid" />
+                        </div>
+                    ))
+                ) : (
+                    <h2>No Images Yet!</h2>
+                )}
+            </div>
         </div>
     );
 };
+
+Images.propTypes = {
+    imagesList: PropTypes.array,
+}
 
 export default Images;

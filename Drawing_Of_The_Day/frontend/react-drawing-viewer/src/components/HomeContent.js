d326ebc993a1c027791dd2052f26593103d865e9
Jack Pflaum <jackpflaum@gmail.com>
2024-01-07 12:36:13 +0800

    added image order option and date option for displaying user images.

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '.components/Header';
-import ImagesGrid from '.components/ImagesGrid';
+import ImagesGrid from '.components/Images';
 
 const HomeContent = () => {
 
@@ -11,25 +11,42 @@ const HomeContent = () => {
         imagesList: [],
     });
 
-    // initiate data fetching of image prompt and images.
-    useEffect(() => {
-        // 'async' keyword specifies that this is an asynchronous function that will return a promise (enables the use of 'await').
-        const fetchData = async () => {
-            try {
-                // 'await' keyword pauses the execution of the function until axios.get promise is resolved.
-                const response = await axios.get('http://localhost:8000/home/');
-                setData(response.data);
-            } catch (error) {
-                console.error('Error fetching data: ', error);
-            }
+    // 'async' keyword specifies that this is an asynchronous function that will return a promise (enables the use of 'await').
+    const fetchData = async (date, orderOption) => {
+        try {
+            // 'await' keyword pauses the execution of the function until axios.get promise is resolved.
+            const response = await axios.get(`http://localhost:8000/home/?date=${date}&order_option=${orderOption}`);
+            setData(response.data);
+        } catch (error) {
+            console.error('Error fetching data: ', error);
         }
-        fetchData();  //calls fetchData function when HomeContent component mounts
+    }
+
+    // initiate data fetching of image prompt and images when HomeContent component mounts.
+    useEffect(() => {
+        fetchData(data.date, 'latest');
     }, []);
+
+    const handleOrderChange = (e) => {
+        const orderOption = e.target.value;
+        fetchdata(data.date, orderOption);
+    };
+
+    const handleDateChange = (e) => {
+        const date = e.target.date;
+        fetchData(date, 'latest');
+    };
 
     return(
         <>
             <Header date={data.date} imagePrompt={data.imagePrompt} />
-            <ImagesGrid imagesList={data.imagesList} />
+            
+            <select name="orderOptions" onChange={handleOrderChange}>
+                <option value="latest">Latest</option>
+                <option value="oldest">Oldest</option>
+            </select>
+            <input type="date" name="date" value={data.date} onChange={handleDateChange} />
+            <Images imagesList={data.imagesList} />
         </>
     );
 };

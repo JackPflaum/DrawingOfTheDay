82aecea075defdf8200af6b1bfa920c41047740f
Jack Pflaum <jackpflaum@gmail.com>
2024-01-08 18:40:49 +0800

    fixed Route Layout indentation and added NoMatch component

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/App.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/App.js
@@ -3,11 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomeContent from './components/HomeContent';
 
+import NoMatch from './components/NoMatch';
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} />
-        <Route path='home' element={<HomeContent />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<HomeContent />} />    {/* this is the default child route when '/' path is rendered */}
+        <Route path='*' element={<NoMatch />} />
+      </Route>
     </Routes>
   );
 }

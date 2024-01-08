af5aeeb4d5a4b987c9bc9f6c9ba2100ffa292932
Jack Pflaum <jackpflaum@gmail.com>
2024-01-08 14:18:22 +0800

    updated 'include' statement to reflect the correct file path

--- Drawing_Of_The_Day/Drawing_Of_The_Day/urls.py
+++ Drawing_Of_The_Day/Drawing_Of_The_Day/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('drawings.urls')),
+    path('/', include('backend.drawings.urls')),
 ]

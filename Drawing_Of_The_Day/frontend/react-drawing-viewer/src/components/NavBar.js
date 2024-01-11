f9563254f79f8eacb5511c9aadc0a55dcd1ebfc7
Jack Pflaum <jackpflaum@gmail.com>
2024-01-11 18:47:04 +0800

    dynamically mapped navigation links across navbar

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NavBar.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NavBar.js
@@ -1,9 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    {path: '/', text: 'Home'},
+    {path: '/login', text: 'Login'},
+    {path: '/logout', text: 'Logout'},
+];
+
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="">
+                <div>
+                    <img src="" alt="logo" />
+                </div>
+                <div>
+                    <ul>
+                        {links.map((link) => {
+                            return (
+                            <li key={link.text}>
+                                <NavLink to={link.path}>{link.text}</NavLink>
+                            </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
-
+    
 export default NavBar;

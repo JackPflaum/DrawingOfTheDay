26e1572d5f6057d49849891d0a162c1453278fc4
Jack Pflaum <jackpflaum@gmail.com>
2024-01-11 18:48:28 +0800

    added PropTypes to Header component

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Header.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Header.js
@@ -1,9 +1,17 @@
-const Header = (prop) => {
+import PropTypes from 'prop-types';
+
+const Header = ({ date, imagePrompt }) => {
     return (
         <>
-            <h2>Todays Prompt: {prop.date}</h2>
-            <p>{prop.imagePrompt}</p>
+            <h2>Todays Date: {date}</h2>
+            <p>Prompt Text: {imagePrompt}</p>
         </>
-    )
-}
+    );
+};
+
+Header.propTypes = {
+    date: PropTypes.string,
+    imagePrompt: PropTypes.string,
+};
+
 export default Header;

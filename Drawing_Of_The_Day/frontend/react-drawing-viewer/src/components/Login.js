a14409989fe7d017cd0c74ffcf326738c9fc7bdb
Jack Pflaum <jackpflaum@gmail.com>
2024-01-12 18:38:00 +0800

    changed local storage of authentication tokens to cookie storage for better security

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Login.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Login.js
@@ -8,17 +8,22 @@ const Login = () => {
         password: '',
     });
 
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8000/api/token/', loginData);
+            const response = await axios.post('http://localhost:8000/token/', loginData);
 
-            // API returns access token and it's saved to local storage for future requests
+            // API returns access token and it's saved to HttpOnly, Secure, and SameSite cookie for greater security.
+            // HttpOnly means the cookie is inaccessible to Javascript and therefore, reducing XSS attacks.
+            // Secure means the cookie is only sent over HTTPS.
+            // SameSite=Strict means the cookie is only sent to the server if the request originates from the site that set the cookie (mitigates CSRF attacks).
             const accessToken = response.data.access;
-            localStorage.setItem('accessToken', accessToken);
+            document.cookie = `accessToken=${accessToken}; HttpOnly; Secure; SameSite=Strict`;
 
             // redirect to home page
-            useNavigate('/');
+            navigate('/');
 
         } catch (error) {
             console.error('Login failed: ', error);

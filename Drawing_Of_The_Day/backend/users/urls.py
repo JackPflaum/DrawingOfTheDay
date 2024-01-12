d01967c0c93c351e2281c41d1d4100c47b748864
Jack Pflaum <jackpflaum@gmail.com>
2024-01-12 18:17:37 +0800

    added signup and logout url paths

--- Drawing_Of_The_Day/backend/users/urls.py
+++ Drawing_Of_The_Day/backend/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from users.views import views
 from rest_framework_simpleJWT import views as jwt_views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]

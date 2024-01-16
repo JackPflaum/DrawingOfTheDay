ef26b16dd7ad6a3d0325d20dc23ff75e8f38d9f2
Jack Pflaum <jackpflaum@gmail.com>
2024-01-16 17:41:50 +0800

    fixed 'check-auth' path

--- Drawing_Of_The_Day/backend/users/urls.py
+++ Drawing_Of_The_Day/backend/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('check-auth', views.check_authorization, name='check_authorization'),
+    path('check-auth/', views.check_authorization, name='check_authorization'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),

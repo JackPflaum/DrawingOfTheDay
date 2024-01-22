baf7ae22a826695855e57816ca67da9b0f6c0d5a
Jack Pflaum <jackpflaum@gmail.com>
2024-01-22 17:28:59 +0800

    added Like serializer for likes and dislikes count on images

--- Drawing_Of_The_Day/backend/drawings/serializers.py
+++ Drawing_Of_The_Day/backend/drawings/serializers.py
@@ -8,6 +8,19 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'url', 'user']
 
 
+class LikeSerializer(serializers.ModelSerializer):
+    # 'likes_count' automatically associated with 'get_likes' method because of naming convention
+    likes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Image
+        fields = ['likes_count']
+
+    def get_likes_count(self, obj):
+        """calculate likes and dislikes count for the associated image"""
+        return obj.count_likes()
+
+
 class ImagePromptSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImagePrompt

7bec0a00e48f4f2df4337ff8d09b33f8054ad5ee
Jack Pflaum <jackpflaum@gmail.com>
2024-01-22 17:30:53 +0800

    added model method for counting image likes and dislikes

--- Drawing_Of_The_Day/backend/drawings/models.py
+++ Drawing_Of_The_Day/backend/drawings/models.py
@@ -18,12 +18,21 @@ class Image(models.Model):
 
     def __str__(self):
         return f'{self.user} uploaded image on {self.upload_date}'
+    
+    def count_likes(self):
+        """counts the likes and dislikes based on reverse relationship with Like model"""
+        # 'like_set' is the default reverse relationship name
+        # accessing the 'like_status' field in the 'Like' model using relationship name
+        likes_count = self.like_set.filter(like_status=True).count()    # True represents a like
+        dislikes_count = self.like_set.filter(like_status=False).count()    # False represents a dislike
+        return {'likes': likes_count, 'dislikes': dislikes_count}
 
 
 class Like(models.Model):
+    """Like model for holding users likes and dislikes of uploaded images"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    like_status = models.BooleanField()
+    like_status = models.BooleanField()    # true represents like and false represents dislikes
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

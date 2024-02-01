608d4a42f148104df019d3276532607ae63b77c7
Jack Pflaum <jackpflaum@gmail.com>
2024-02-01 17:56:46 +0800

    formated date so the month is in word format and removed '-'

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Header.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Header.js
@@ -1,9 +1,26 @@
 import PropTypes from 'prop-types';
+import  { useEffect, useState } from 'react';
+import { format, parse } from 'date-fns';
 
 const Header = ({ date, imagePrompt }) => {
+    const [ datePattern, setDatePattern ] = useState(date);
+
+    useEffect(() => {
+        // update date format so the month is in word format
+        const formatDate = (newDate) => {
+            // parse used to convert date string into date object
+            const parsedDate = parse(newDate, 'dd-MM-yyyy', new Date());
+    
+            // format the parsed date as 'dd MMMM yyy' e.g 31 January 2024
+            const newDatePattern = format(parsedDate, 'dd MMMMMM yyyy');
+            setDatePattern(newDatePattern);
+        }
+        formatDate(date);
+    }, [date]);
+
     return (
-        <div className="text-center">
-            <h4>Todays Date: {date}</h4>
+        <div className="text-center mt-3">
+            <h4>{datePattern}</h4>
             <p>Prompt Text: {imagePrompt}</p>
         </div>
     );

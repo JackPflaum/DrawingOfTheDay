c080f16364b12cbb609566e56dd7d3f1de8d1ce2
Jack Pflaum <jackpflaum@gmail.com>
2024-02-01 17:31:43 +0800

    formatted date so it is read as dd-MM-yyyy instead of yyyy/MM/dd

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/HomeContent.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { axiosResponseInstance } from '../api/axiosResponseInstance';
+import axios from 'axios'
 import Header from './Header';
 import Images from './Images';
 import UploadModal from './UploadModal';
-import { useOnClickOutside } from '../customHooks/useOnClickOutside';
+import { useOnClickOutside } from '../hooks/customHooks';
+import { format } from 'date-fns';
 
 const HomeContent = () => {
+    // for opening up Upload Image modal
     const [ openModal, setOpenModal ] = useState(false);
     const ref = useRef();
 
+    // set intial date to today and use date-fns format function to format the date
+    const currentDate = new Date();
+    // replace default separator '/' with '-' so it can be sent as a html parameter
+    const formattedDate = format(currentDate, 'dd-MM-yyyy');
+
     const [ data, setData ] = useState({
-        date: new Date().toISOString().split('T')[0],  //set inital date to today
+        date: formattedDate,
         imagePrompt: '',
         imagesList: [],
         orderOption: '-upload_date',
@@ -19,9 +26,26 @@ const HomeContent = () => {
     // 'async' keyword specifies that this is an asynchronous function that will return a promise (enables the use of 'await').
     const fetchData = async (date, orderOption) => {
         try {
+            // format date as 'yyyy-MM-dd' because that is how backend requires it.
+            const [ day, month, year ] = date.split('-');
+            const formattedDateForBackend = format(new Date(`${year}-${month}-${day}`), 'yyyy-MM-dd');
+
             // 'await' keyword pauses the execution of the function until axios.get promise is resolved.
-            const response = await axiosResponseInstance.get(`http://localhost:8000/home/?date=${date}&order_option=${orderOption}`);
-            setData(response.data);
+            const response = await axios.get(`http://localhost:8000/home/?date=${formattedDateForBackend}&order_option=${orderOption}`);
+
+            // convert returned date to 'dd-MM-yyyy'
+            const returnedDate = response.data.date;
+            const [ returnedYear, returnedMonth, returnedDay ] = returnedDate.split('-');
+            const formattedDateForFrontend = `${returnedDay}-${returnedMonth}-${returnedYear}`;
+            
+            // set the data that was received from api request
+            setData({
+                date: formattedDateForFrontend,
+                imagePrompt: response.data.imagePrompt,
+                imagesList: response.data.imagesList,
+                orderOption: response.data.orderOption,
+            });
+
         } catch (error) {
             console.error('Error fetching data: ', error);
         }
@@ -34,15 +58,23 @@ const HomeContent = () => {
 
     const handleDateChange = (e) => {
         e.preventDefault();
-        const date = e.target.value;
+
+        // convert date string to an object which allows the date to be formatted in the desired way.
+        const dateString = e.target.value;
+        const dateObject = new Date(dateString);
+
+        // use date-fns format function to format the date
+        const formattedDate = format(dateObject, 'dd-MM-yyyy');
+        
         setData((prev) => ({
             ...prev,
-            date: date,
+            date: formattedDate,
         }));
     };
 
     const handleOrderChange = (e) => {
         e.preventDefault();
+        
         const orderOption = e.target.value;
         setData((prev) => ({
             ...prev,
@@ -65,15 +97,15 @@ const HomeContent = () => {
         <div className={openModal ? "container backdrop" : "container"}>
             <Header date={data.date} imagePrompt={data.imagePrompt} />
             <div className="d-flex justify-content-between">
-                <button onClick={openUploadModal}>Upload Drawing</button>
+                <button className="btn btn-primary" onClick={openUploadModal}>Upload Drawing</button>
                 { openModal && <UploadModal ref={ref} isOpen={openModal} closeModal={closeUploadModal} />}
 
-                <div className="d-inline-flex">
-                    <select name="orderOptions" onChange={handleOrderChange}>
+                <div className="d-flex">
+                    <select name="orderOptions" className="me-2" onChange={handleOrderChange}>
                         <option value="-upload_date">Latest</option>
                         <option value="upload_date">Oldest</option>
                     </select>
-                    <input type="date" name="date" value={data.date} onChange={handleDateChange} />
+                    <input type="date" name="date" className="pr-4" value={data.date} onChange={handleDateChange} />
                 </div>
 
             </div>

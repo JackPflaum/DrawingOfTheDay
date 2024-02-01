d9195270d2498517b3b845b6feca0190c1c4fedf
Jack Pflaum <jackpflaum@gmail.com>
2024-02-01 18:22:40 +0800

    reformatted NoMatch component UI to look better and added link back to home

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NoMatch.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NoMatch.js
@@ -1,7 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import { IoHomeOutline } from "react-icons/io5";
+
 const NoMatch = () => {
     return (
-        <div>
-            <h2>Not Found!</h2>
+        <div className="container">
+            <div className="row text-center pt-5">
+                <h1>Oops!</h1>
+                <h2>404 Page Not Found!</h2>
+                <div className="">
+                    Sorry, an error has occured
+                </div>
+                <div className="mt-2">
+                    <NavLink to="/" className="btn btn-primary"><IoHomeOutline /> Home</NavLink>
+                </div>
+            </div>
         </div>
     );
 };

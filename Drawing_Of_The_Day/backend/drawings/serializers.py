49726bba98648feace220c4d68e80475b11ef9d4
Jack Pflaum <jackpflaum@gmail.com>
2024-02-08 17:50:49 +0800

    added ImageSerializer methods to return image url and username.

--- Drawing_Of_The_Day/backend/drawings/serializers.py
+++ Drawing_Of_The_Day/backend/drawings/serializers.py
@@ -3,22 +3,32 @@ from .models import Image, ImagePrompt
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
-        fields = ['id', 'url', 'user']
+        fields = ['id', 'user', 'url', 'username']
+
+    def get_url(self, obj):
+        return obj.image.url if obj.image else None
+    
+    def get_username(self, obj):
+        """get username from foreignkey connection with User model"""
+        return obj.user.username if obj.user else None
 
 
 class LikeSerializer(serializers.ModelSerializer):
     # 'likes_count' automatically associated with 'get_likes' method because of naming convention
-    likes_count = serializers.SerializerMethodField()
+    likes_dislikes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Image
-        fields = ['likes_count']
+        fields = ['likes_dislikes_count']
 
-    def get_likes_count(self, obj):
+    def get_likes_dislikes_count(self, obj):
         """calculate likes and dislikes count for the associated image"""
-        return obj.count_likes()
+        return obj.count_likes_dislikes()
 
 
 class ImagePromptSerializer(serializers.ModelSerializer):

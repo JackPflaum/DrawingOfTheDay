b4b9f6f2676416cfe55efda64af0d569d28180ff
Jack Pflaum <jackpflaum@gmail.com>
2024-02-12 19:50:22 +0800

    created image_likes and like_dislike views

--- Drawing_Of_The_Day/backend/drawings/urls.py
+++ Drawing_Of_The_Day/backend/drawings/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('upload-image/', views.upload_image, name='upload_image'),
+    path('image-likes/', views.image_likes, name='image_likes'),
+    path('like-dislike/', views.like_dislike, name='like_dislike'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    # serves uploaded media files during development
--- Drawing_Of_The_Day/backend/drawings/views.py
+++ Drawing_Of_The_Day/backend/drawings/views.py
@@ -1,8 +1,11 @@
-from datetime import date, datetime, timezone
+from datetime import date, datetime, timedelta
+from django.core.exceptions import ValidationError, PermissionDenied
+from django.utils import timezone
 from .models import ImagePrompt, Image, Like
 from rest_framework.response import Response
-from .serializers import ImageSerializer, ImagePromptSerializer
-from rest_framework.decorators import api_view, parser_classes
+from .serializers import ImageSerializer, LikeSerializer
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 
@@ -20,16 +23,17 @@ def home(request):
     # '__date' only filters the images based on date and not date and time.
     images = Image.objects.filter(upload_date__date=date_str).order_by(order_option)
     images_data = ImageSerializer(images, many=True).data
+    print('images data:', images_data)
 
     # get likes and dislikes count from Like model count function?
 
-    # get image prompt text for particular date
+    # get image prompt object for particular date and extract prompt_text
     image_prompt = ImagePrompt.objects.filter(date=date_str).first()
-    image_prompt_data = ImagePromptSerializer(image_prompt).data if image_prompt else None
+    image_prompt_data = image_prompt.prompt_text if image_prompt else None
 
     context = {'date': date_str,
                'imagePrompt': image_prompt_data if image_prompt_data else 'Sorry, No Image Prompt Today.',
-               'images': images_data,
+               'imagesList': images_data,
                'orderOption': order_option,
                }
     
@@ -37,22 +41,109 @@ def home(request):
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 @parser_classes([MultiPartParser, FormParser])    # using parsers to handle both form submissions and file uploads
 def upload_image(request):
     """uploads user image to database"""
-
     try:
-        # get the image file and image text prompt using current date
-        image = request.FILES.get('imageFile')
-        date = timezone.now().date()
-        image_prompt = ImagePrompt.objects.filter(date=date).first()
+        # check if user has already submitted an image today
+        user = request.user
+        current_date = timezone.now()
+        start_of_day = timezone.make_aware(timezone.datetime(current_date.year, current_date.month, current_date.day))
+        end_of_day = timezone.make_aware(timezone.datetime(current_date.year, current_date.month, current_date.day, 23, 59, 59, 999999))
 
-        if not image_prompt:
-            return Response({'error': 'Image Prompt not found for given date.'}, status=status.HTTP_404_NOT_FOUND)
+        print("start of day: ", start_of_day)
+        print("end of day: ", end_of_day)
+        # checking date with __range because upload_date is DateTimeField and we need to take into account date and time.
+        already_uploaded = Image.objects.filter(user=user, upload_date__range=(start_of_day, end_of_day)).exists()
+
+
+        # already_uploaded = Image.objects.filter(user=user, 
+        #     upload_date__range=(datetime.datetime.combine(start_date, datetime.time.min), datetime.datetime.combine(end_date, datetime.time.max),)).exists()
+        print("Already Uploaded:", already_uploaded)
+        if already_uploaded:
+            return Response({'error': 'Sorry, you can only submit one drawing per day.'}, status=status.HTTP_409_CONFLICT)
+
+        # get the image file and check file size is within set limit
+        image = request.FILES.get('imageFile')
+
+        megabyte_limit = 5
+        if image.size > megabyte_limit * 1024 * 1024:
+            raise ValidationError('Image size exceeds the allowable limit of 5MB')
+
+        # get image text prompt using current date
+        image_prompt = ImagePrompt.objects.get(date=date)
         
         # save image file to Image model
         Image.objects.create(image_prompt=image_prompt, user=request.user, image=image)
         return Response({'success': 'Image uploaded successfully'}, status=status.HTTP_201_CREATED)
+
+    except PermissionDenied:
+        return Response({'error': 'Unauthorized access'}, status=status.HTTP_401_UNAUTHORIZED)
+    except ImagePrompt.DoesNotExist:
+        return Response({'error': 'Image Prompt not found for given date.'}, status=status.HTTP_404_NOT_FOUND)
+    except ValidationError as validation_error:
+        return Response({'error': str(validation_error)}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as error:
-        print(error)
-        return Response({'error': 'Error uploading image'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        print(f'error loading image: {error}')
+        return Response({'error': 'Image was not uploaded due to internal server error:',}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def image_likes(request):
+    """get image likes and dislikes count. If user is logged in and has already liked or dislikes someones image
+    then send back true"""
+    image_likes = []
+
+    # retrieve image ids from request paramaters
+    image_ids = request.GET.getlist('image_ids')
+
+    # append likes data to image ids
+    for image_id in image_ids:
+        image = Image.objects.filter(id=id).first()
+        like_dislike_count = LikeSerializer(image, context={'request': request}).data
+        image_likes.append({image_id: like_dislike_count})
+
+    return Response(image_likes, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_dislike(request):
+    """save users like or dislike response to an image"""
+    print('made it to like_dislike view')
+    try:
+        user = request.user
+        like_status = request.data.get('likeStatus')
+        image_id = request.data.get('imageId')
+
+        print('like_status ', like_status)
+
+        # get image that is being liked or disliked.
+        image = Image.objects.get(id=image_id)
+
+        # check if user has already liked or disliked the image and overwrite previous like_status if so.
+        like = Like.objects.filter(user=user, image=image).first()
+
+        if like is not None:
+            # null means user is taking away their previous like or dislike response.
+            if like_status is None:
+                like.delete()
+                return Response({'success': 'Like status deleted successfully'}, status=status.HTTP_200_OK)
+            else:
+                like.like_status = like_status
+                like.save()
+                return Response({'success': 'Like status successfully submitted'}, status=status.HTTP_201_CREATED)
+
+        # if user has not liked or disliked image before then save like_status to Like model.
+        Like.objects.create(user=user, image=image, like_status=like_status)
+        return Response({'success': 'Like status successfully submitted'}, status=status.HTTP_201_CREATED)
+
+    except PermissionDenied:
+        return Response({'error': 'Unauthorized access'}, status=status.HTTP_401_UNAUTHORIZED)
+    except Image.DoesNotExist:
+        return Response({'error': 'Image not found for given id'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as error:
+        print(f'Error: {error}')
+        return Response({'error': 'Like or dislike response was not submitted due to internal server error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+

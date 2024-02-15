de146e5b66a93e9142f0f673a217f738e6645f2e
Jack Pflaum <jackpflaum@gmail.com>
2024-02-15 18:55:55 +0800

    added model fields to list_display in Admin

--- Drawing_Of_The_Day/backend/drawings/admin.py
+++ Drawing_Of_The_Day/backend/drawings/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Image, ImagePrompt, Like
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image_prompt', 'upload_date')
 
-admin.site.register(Image)
-admin.site.register(ImagePrompt)
-admin.site.register(Like)
+
+class ImagePromptAdmin(admin.ModelAdmin):
+    list_display = ('date', 'prompt_text')
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image', 'like_status', 'timestamp')
+
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(ImagePrompt, ImagePromptAdmin)
+admin.site.register(Like, LikeAdmin)

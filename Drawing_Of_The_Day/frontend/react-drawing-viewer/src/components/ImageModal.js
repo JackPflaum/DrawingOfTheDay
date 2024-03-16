426c5b8a5d928653062e07ccb4fed37052254e4b
Jack Pflaum <jackpflaum@gmail.com>
2024-03-16 16:30:17 +0800

    added image download functionality

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/ImageModal.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/ImageModal.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import '../css/ImageModal.css';
+import { saveAs } from 'file-saver';
 
 const ImageModal = ({ showModal, closeModal, imageUrl, username, imagePrompt, date }) => {
+
+  // allows user to download drawing
+  const handleDownloadDrawing = () => {
+    saveAs(`http://localhost:8000/${imageUrl}`, `drawing-${date}.jpg`);
+  }
+
+
   return (
     <Modal show={showModal} onHide={closeModal} size="lg">
       <Modal.Header closeButton>
@@ -16,9 +24,8 @@ const ImageModal = ({ showModal, closeModal, imageUrl, username, imagePrompt, da
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={closeModal}>
-          Close
-        </Button>
+        <Button variant="secondary" onClick={closeModal}>Close</Button>
+        <Button variant="primary" onClick={handleDownloadDrawing}>Download Drawing</Button>
       </Modal.Footer>
     </Modal>
   );

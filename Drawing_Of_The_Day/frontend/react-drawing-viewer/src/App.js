c3467ccc15e239d49a1d82af1216fb6a4f569aee
Jack Pflaum <jackpflaum@gmail.com>
2024-03-19 20:28:40 +0800

    converted login and signup pages into modals.
    Login and signup nav bar links open modals
    instead of redirecting to a new page. Presentation
    looks more appealing and interactive.

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/App.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomeContent from './components/HomeContent';
-import Login from './components/Login';
-import Signup from './components/Signup';
 import NoMatch from './components/NoMatch';
 import UserProfile from './components/UserProfile';
 import ForgotPassword from './components/ForgotPassword';
@@ -15,8 +13,6 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<HomeContent />} />    {/* this is the default child route when '/' path is rendered */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
         <Route path='/profile/:userId' element={<UserProfile />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/password-reset/confirm' element={<PasswordReset />} />
--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NavBar.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/NavBar.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
+import Login from './Login';
+import Signup from './Signup';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { IoIosLogIn, IoIosLogOut } from 'react-icons/io';
@@ -11,6 +14,16 @@ const NavBar = () => {
     const { user, setUser } = useAuthContext();
 
     const navigate = useNavigate();
+
+    // login modal functionality
+    const [ showLoginModal, setShowLoginModal ] = useState(false);
+    const handleOpenLogin = () => setShowLoginModal(true);
+    const handleCloseLogin = () => setShowLoginModal(false);
+
+    // signup modal functionality
+    const [ showSignupModal, setShowSignupModal ] = useState(false);
+    const handleOpenSignup = () => setShowSignupModal(true);
+    const handleCloseSignup = () => setShowSignupModal(false);
 
     const links = [
         {path: '/', text: 'Home'},
@@ -54,12 +67,12 @@ const NavBar = () => {
         // navbar with authorization links only showing under certain logged in conditions
         <nav className="navbar navbar-expand-lg">
             <div className="container">
-                <NavLink to="/" className="navbar-brand nav-heading" onClick={handlePageRefresh}>
+                <NavLink to="/" className="navbar-brand nav-heading zoom" onClick={handlePageRefresh}>
                     <img src="paint_brush_logo.png" width="40" height="40" className="d-inline-block rounded-circle me-2" alt="logo"></img>
                     Drawing Of The Day
                 </NavLink>
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
@@ -68,7 +81,7 @@ const NavBar = () => {
                             return (
                                 <li key={link.text} className="nav-item me-1">
                                     <NavLink to={link.path}
-                                    className="nav-link" 
+                                    className="nav-link custom-link" 
                                     onClick={link.text === 'Home' ? handlePageRefresh : null}>
                                         <h5>{link.text}</h5>
                                     </NavLink>
@@ -76,17 +89,17 @@ const NavBar = () => {
                             )})
                         }
                         { user ? (
-                            // if user is logged in then show logout button and profile link
+                            // if user is logged in then show logout and profile link
                             <>
                                 <li className="nav-item">
-                                    <NavLink onClick={handleLogout} className="nav-link me-1">
+                                    <NavLink onClick={handleLogout} className="nav-link custom-link me-1">
                                         <h5>
                                             <IoIosLogOut className="me-1" />Logout
                                         </h5>
                                     </NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to={`/profile/${user.userId}`} className="nav-link">
+                                    <NavLink to={`/profile/${user.userId}`} className="nav-link custom-link">
                                         <h5>
                                             <IoPersonOutline className="me-1" />Profile
                                         </h5>
@@ -97,18 +110,32 @@ const NavBar = () => {
                             // if user is not logged in then show login and signup links
                             <>
                                 <li className="nav-item">
-                                    <NavLink to="/login" className="nav-link me-1">
+                                    <NavLink onClick={handleOpenLogin} className="nav-link custom-link me1">
                                         <h5>
                                             <IoIosLogIn className="me-1" />Login
                                         </h5>
                                     </NavLink>
+                                    { showLoginModal && (
+                                        <Login
+                                            showLoginModal={showLoginModal}
+                                            handleCloseLogin={handleCloseLogin}
+                                            handleOpenSignup={handleOpenSignup}
+                                        />
+                                    )}
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/signup" className="nav-link">
+                                    <NavLink onClick={handleOpenSignup} className="nav-link custom-link">
                                         <h5>
                                             <IoPersonAddOutline className="me-1" />Signup
                                         </h5>
                                     </NavLink>
+                                    { showSignupModal && (
+                                        <Signup
+                                            showSignupModal={showSignupModal}
+                                            handleCloseSignup={handleCloseSignup}
+                                            handleOpenLogin={handleOpenLogin}
+                                        />
+                                    )}
                                 </li>
                             </>
                         )}

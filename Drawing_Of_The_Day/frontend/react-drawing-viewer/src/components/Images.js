7923a377a9e62eac8170e8c958f0c3fa916b44cb
Jack Pflaum <jackpflaum@gmail.com>
2024-03-24 18:57:18 +0800

    added top margin to image inn bootstrap card

--- Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Images.js
+++ Drawing_Of_The_Day/frontend/react-drawing-viewer/src/components/Images.js
@@ -192,7 +192,7 @@ const Images = ({ imagesList }) => {
                 {imagesState.length > 0 ? (
                     imagesState.map((image) => (
                         <div key={image.id} className="card col-md-3 mb-3 me-4">
-                            <div onClick={() => toggleImageModal(image.id)} className="d-flex align-items-center" style={{ height: '100%' }}>
+                            <div onClick={() => toggleImageModal(image.id)} className="d-flex align-items-center mt-2" style={{ height: '100%' }}>
                                 <img src={`http://localhost:8000/${image.url}`} alt={image.id} className="img-fluid" />
                             </div>
                             {imageModalStates[image.id] && (<ImageModal
